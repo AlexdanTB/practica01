b0820226504ea3beb95486cd91e63c3588c8523d
Jav2004ier <156093114+Jav2004ier@users.noreply.github.com>
2024-11-02 11:29:10 -0500

    primer comentario

---
+++ Mavenproject1.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ItsqmetEstudiantes
+ */
+public class Mavenproject1 {
+
+    public static void main(String[] args) {
+        
+        // Calcular area de triangulo
+        
+        double baset;
+        double alturat;
+        double respuesta;
+        
+        double radioc;
+        double areac;
+        double pi = 3.1415;
+        
+        Scanner ingreso = new Scanner(System.in);
+        System.out.println("Ingresar la base del triangulo");
+        
+        baset = ingreso.nextDouble();
+        
+        System.out.println("Ingresar la altura del triangulo");
+        
+        alturat = ingreso.nextDouble();
+        
+        respuesta = (baset*alturat)/2;
+        System.out.println("El area del triangulo es igual a "+respuesta+ "cm2");
+        System.out.println("--------------------------------------------");
+        
+        // Calcular area del circulo
+        System.out.println("Ingresar el radio del circulo");
+        radioc = ingreso.nextDouble();
+        
+        areac = pi*(radioc*radioc);
+        
+        System.out.println("El area del circulo es igual a "+areac+"cm2");
+        
+    }
+}
